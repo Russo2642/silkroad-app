89e24cc6d1cfd865df092cb7165b2c9796361c14
Ruslan Zulufov <gothiq2302@gmail.com>
2024-10-28 22:40:14 +0500

    fix add photos a

--- internal/repository/pg/tour_photos.go
+++ internal/repository/pg/tour_photos.go
@@ -54,7 +54,7 @@ func (r *TourPostgres) AddPhotos(tourID int, files []*multipart.FileHeader) erro
 		}
 	}
 
-	updatePhotosQuery := fmt.Sprintf("UPDATE %s SET photos = $1 WHERE id = $2", tourTable)
+	updatePhotosQuery := fmt.Sprintf("UPDATE %s SET photos = array_cat(COALESCE(photos, ARRAY[]::text[]), $1) WHERE id = $2", tourTable)
 	_, err = tx.Exec(updatePhotosQuery, pq.Array(photoUrls), tourID)
 	if err != nil {
 		_ = tx.Rollback()
